64ea37f813b3829d618c180b10ba72b972a0e8c6
timcoulter <Cricket8>
2018-04-13 09:57:19 +1000

    prac_07

--- prac_07/languages.py
+++ prac_07/languages.py
@@ -1,2 +1,13 @@
 from programming_language import ProgrammingLanguage
-ruby = ProgrammingLanguage("Ruby", "Dynamic", )
+languages = [ProgrammingLanguage('Ruby', 'Dynamic', True, 1995), ProgrammingLanguage('Python', 'Dynamic', True, 1991),
+             ProgrammingLanguage('Visual Basic', 'Static', False, 1991)]
+for language in languages:
+    print(language)
+
+print('The dynamically typed languages are: ')
+for language in languages:
+    if language.is_dynamic():
+        print(language.data[0])
+
+
+
--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -2,20 +2,14 @@ import inspect
 
 class ProgrammingLanguage:
     def __init__(self, name=None, typing=None, reflection=None, year=None):
-        self.name = name.title()
-        self.typing = typing.strip().lower()
-        self.reflection = reflection.lower().title().strip()
-        self.year = year
+        self.data = [name.title(), typing.strip().lower().title(), reflection, year]
 
     def __str__(self):
-        return str('{}, {} Typing, Reflection={}, First appeared in {}'.format(self.name), self.typing, self.reflection, self.year )
+        return str('{0}, {1} Typing, Reflection={2}, First appeared in {3}'.format(self.data[0], self.data[1], self.data[2], self.data[3]))
 
     def is_dynamic(self):
-        typing = self.typing
-        if typing == 'dyanmic':
-            print('{} is Dynamic!'.format(self.name))
+        typing = self.data[1]
+        if typing == 'Dynamic':
+            return True
         else:
-            print('{} is not dynamic :('.format(self.name))
-
-
-
+            return False
