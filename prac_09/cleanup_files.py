d5f46ab2bddffec618e6254f089dff95f95a6236
timcoulter <timothy.coulter@my.jcu.edu.au>
2018-05-08 10:08:40 +1000

    prac_09 progress

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -4,6 +4,7 @@ File renaming and os examples
 """
 import shutil
 import os
+import heapq
 
 
 def main():
@@ -19,41 +20,23 @@ def main():
         print('Files in the directory >>> {0}'.format(filename))
         for subfiles in os.listdir(filename):
             print(subfiles)
+            subfiles = get_fixed_filename(subfiles)
+            print(subfiles)
         if not os.path.isdir(filename):
             new_name = get_fixed_filename(filename)
             print(new_name)
 
-            # NOTE: These options won't just work...
-            # they show you ways of renaming and moving files,
-            # but you need to have valid filenames. You can't make a file with
-            # a blank name, and on Windows you can't have files with the same
-            # characters but different case (e.g. a.TXT and A.txt)
-            # So, you need to get valid filenames before you can use these.
-
-            # Option 1: rename file to new name - in place
-            # os.rename(filename, new_name)
-
-            # Option 2: move file to new place, with new name
-            # shutil.move(filename, 'temp/' + new_name)
-
-            # Processing subdirectories using os.walk()
-
-            # os.chdir('..')  # .. means "up" one directory
-            # for dir_name, subdir_list, file_list in os.walk('.'):
-            #     print("In", dir_name)
-            #     print("\tcontains subdirectories:", subdir_list)
-            #     print("\tand files:", file_list)
-
-
 def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-    # First, replace the spaces and .TXT (the easy part)
-    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
-
     new_name = ""
-    # TODO: step-by-step, consider the problem cases and solve them
-
+    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
+    for i, char in enumerate(filename, 0):
+        last_char = i-1
+        if i == 0 or filename[last_char] == '_' or filename[last_char] == '(':
+            new_name += char.upper()
+        elif char.isupper() and filename[last_char] != '_':
+            new_name += ('_' + char)
+        else:
+            new_name += char
     return new_name
-
 
 main()
